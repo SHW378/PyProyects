906f6c9460bde11a8e17b2965919240395dcb643
SHW378 <cesarbrmn@hotmail.com>
2025-08-28 19:25:03 -0600

    libreria math py

---
+++ import Math.py
@@ -0,0 +1,9 @@
+import math
+
+ladoa = 4
+print("el lado a es: " + str(ladoa))
+ladob = 6;
+print("el lado b es: " + str((ladob)))
+
+c = math.sqrt(math.pow(ladoa, 2) + math.pow(ladob, 2))
+print("la longitud de la hipotenusa es: " + str(c))
