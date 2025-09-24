92b98dc2883b0ba91406a2e9b332ce2c1899a3f8
SHW378 <cesarbrmn@hotmail.com>
2025-09-23 18:33:23 -0600

    s/n

--- promediopython.py
+++ promediopython.py
@@ -11,10 +11,10 @@ while True:
     except ValueError:
         print("Por favor ingrese un número válido.")
         continue
-    decision = input("¿Desea continuar? (si/no): ")
-    while decision != "si" and decision != "no":
-        print("Opción inválida. Por favor ingrese 'si' o 'no'.")
-        decision = input("¿Desea continuar? (si/no): ")
-    if decision == "no":
+    decision = input("¿Desea continuar? (s/n): ")
+    while decision != "s" and decision != "n":
+        print("Opción inválida. Por favor ingrese 's' o 'n'.")
+        decision = input("¿Desea continuar? (s/n): ")
+    if decision == "n":
         break
 print("El promedio es:", promedio)
