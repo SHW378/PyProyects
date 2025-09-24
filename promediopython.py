7ac8124671d5592e0313a7579f69b99fcb273499
SHW378 <cesarbrmn@hotmail.com>
2025-09-23 18:32:52 -0600

    debugeado

--- promediopython.py
+++ promediopython.py
@@ -4,18 +4,14 @@ suma = 0
 decision = ""
 while True:
     try:
-        numero = int(input("Ingrese una edad: "))
+        numero = float(input("Ingrese un número: "))
         suma += numero
         cantidad += 1
         promedio = suma / cantidad
     except ValueError:
         print("Por favor ingrese un número válido.")
         continue
-    try:
-        decision = input("¿Desea continuar? (si/no): ")
-    except ValueError:
-        print("Por favor ingrese 'si' o 'no'.")
-        continue
+    decision = input("¿Desea continuar? (si/no): ")
     while decision != "si" and decision != "no":
         print("Opción inválida. Por favor ingrese 'si' o 'no'.")
         decision = input("¿Desea continuar? (si/no): ")
